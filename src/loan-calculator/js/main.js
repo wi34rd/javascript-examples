cdbfe44d715d84ad0fc101394aea8beea2ad38d6
wi34rd <wi34rd@gmail.com>
2019-05-02 16:18:18 +0700

    Rafactors code

--- src/loan-calculator/js/main.js
+++ src/loan-calculator/js/main.js
@@ -1,45 +1,45 @@
 class LoanCalculator {
-    constructor() {
-        this._cardBlock = document.querySelector('.card');
-        this._cardHeadingBlock = this._cardBlock.querySelector('.heading');
-        this._interestInput = document.getElementById('interest-input');
-        this._amountInput = document.getElementById('amount-input');
-        this._yearsInput = document.getElementById('years-input');
-        this._monthlyPaymentInput = document.getElementById('monthly-payment-input');
-        this._totalPaymentInput = document.getElementById('total-payment-input');
-        this._totalInterestInput = document.getElementById('total-interest-input');
+    card = document.getElementById('card');
+    loanForm = document.getElementById('loan-form');
+    interestInput = document.getElementById('interest-input');
+    amountInput = document.getElementById('amount-input');
+    yearsInput = document.getElementById('years-input');
+    monthlyPaymentInput = document.getElementById('monthly-payment-input');
+    totalPaymentInput = document.getElementById('total-payment-input');
+    totalInterestInput = document.getElementById('total-interest-input');
 
-        document.getElementById('loan-form').addEventListener('submit', (event) => this._onLoanFormSubmit(event));
+    constructor() {
+        this.loanForm.addEventListener('submit', (event) => this.onLoanFormSubmit(event));
     }
 
-    _onLoanFormSubmit(event) {
+    onLoanFormSubmit(event) {
         event.preventDefault();
 
-        const principal = parseFloat(this._amountInput.value);
-        const calculatedInterest = parseFloat(this._interestInput.value) / 100 / 12;
-        const calculatedPayments = parseFloat(this._yearsInput.value) * 12;
-
+        const principal = parseFloat(this.amountInput.value);
+        const calculatedInterest = parseFloat(this.interestInput.value) / 100 / 12;
+        const calculatedPayments = parseFloat(this.yearsInput.value) * 12;
         const x = Math.pow(1 + calculatedInterest, calculatedPayments);
         const monthlyPayment = (principal * x * calculatedInterest) / (x - 1);
 
         if (isFinite(monthlyPayment)) {
-            this._monthlyPaymentInput.value = monthlyPayment.toFixed(2);
-            this._totalPaymentInput.value = (monthlyPayment * calculatedPayments).toFixed(2);
-            this._totalInterestInput.value = ((monthlyPayment * calculatedPayments) - principal).toFixed(2);
+            this.monthlyPaymentInput.value = monthlyPayment.toFixed(2);
+            this.totalPaymentInput.value = (monthlyPayment * calculatedPayments).toFixed(2);
+            this.totalInterestInput.value = ((monthlyPayment * calculatedPayments) - principal).toFixed(2);
         } else {
             this.showError('Пожалуйста проверьте введенные вами данные.');
         }
     }
 
     showError(errorText) {
-        const errorBlock = document.createElement('div');
-        errorBlock.className = 'alert alert-danger';
-        errorBlock.appendChild(document.createTextNode(errorText));
-        this._cardBlock.insertBefore(errorBlock, this._cardHeadingBlock);
+        const errorMessage = document.createElement('div');
+
+        errorMessage.className = 'alert alert-danger';
+        errorMessage.appendChild(document.createTextNode(errorText));
+        this.card.insertBefore(errorMessage, this.loanForm);
         setTimeout(() => {
-            this._cardBlock.querySelector('.alert').remove();
+            errorMessage.remove();
         }, 3000);
     }
 }
 
-new LoanCalculator();
+loanCalculator = new LoanCalculator();
